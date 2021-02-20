a53558ddb78cb6e4958613f78c7461d43f8da30d
bspencer97 <bspencer16@apu.edu>
2021-02-20 15:30:43 -0800

    Added login and profile pages, and user component

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Login = () => (
+    <Layout>
+        <p>Login here</p>
+    </Layout>
+)
+
+export default Login
---
+++ src/pages/profile.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Profile = () => (
+    <Layout>
+        <p>User profile</p>
+    </Layout>
+)
+
+export default Profile
